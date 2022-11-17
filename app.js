d163ca5a194f1e0d7d8ee5d29680e3c717f87d26
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-11-17 21:39:18 +0000

    CORS added with .env variables

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import userRouter from "./routes/users.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = [process.env.DEV_FRONTEND, process.env.PROD_FRONTEND];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 
